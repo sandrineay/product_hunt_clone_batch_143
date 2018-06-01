255e571997db66f7bb0bfda3cc5f96cc060efd58
Sandrine Ayral <sandrine.ayral@gmail.com>
2018-06-01 10:35:23 +0100

    Travis setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def full_name
+    if first_name.nil?
+      last_name.capitalize
+    else
+      "#{first_name.capitalize} #{last_name.capitalize}"
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "john", last_name: "lennon")
+    assert_equal "John Lennon", user.full_name
+  end
+
+  test "full_name does not crash if first_name is nil" do
+    user = User.new(last_name: "lennon")
+    assert_equal "Lennon", user.full_name
+  end
 end
